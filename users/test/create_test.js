b311f9ef1159fe9b40ae2b23765b320d1df79e07
Sean Hong <iamseanhong@gmail.com>
2018-02-23 20:48:26 -0800

    First assert testing saving user

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -2,8 +2,13 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Createing records', () => {
-  it('saves a user', () => {
+  it('saves a user', (done) => {
     const sean = new User({ name: 'Sean'});
-    sean.save();
+    sean.save()
+      .then(() => {
+        assert(!sean.isNew);
+        done();
+      });
+    
   })
 })
