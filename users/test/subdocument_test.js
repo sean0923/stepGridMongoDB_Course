999c0795936061e1008c7d1a103cf1a472ed6a9d
Sean Hong <iamseanhong@gmail.com>
2018-02-25 21:32:11 -0800

    Add subdocument for an exsisting user

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const User = require('../src/user');
 
+
+
 describe('Subdocument test', () => {
   it('can create a subdocument', (done) => {
     const joe = new User ({
@@ -16,5 +18,23 @@ describe('Subdocument test', () => {
         assert(user.posts[0].title === 'PostTitle');
         done();
       })
-  })
+  });
+
+  it('Add subdocument for an exsiting user', (done) => {
+    const joe = new User({
+      name: 'joe',
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'joe'}))
+      .then((user) => {
+        return user.update({ posts: [{ title: 'PostTitle' }], })
+      })
+      .then(() => User.findOne({ name: 'joe' }))
+      .then((user) => {
+        assert(user.posts[0].title === 'PostTitle');
+        done();
+      });
+      
+  });
 })
