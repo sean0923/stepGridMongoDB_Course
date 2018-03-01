b0da4a805409e88b72fd7489874da61d0216b367
Sean Hong <iamseanhong@gmail.com>
2018-02-28 20:39:37 -0800

    First relational data test

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -4,29 +4,26 @@ const BlogPost = require('../src/blogPost');
 const Comment = require('../src/comment');
 
 describe('Association test', () => {
-  let joe;
-  let blogPost;
-  let comment;
+  let joe, blogPost, comment;
 
-  beforeEach(done => {
+  beforeEach((done) => {
     joe = new User({ name: 'joe' });
-    blogPost = new BlogPost({ title: 'blogPostTitle', content: 'blogPostContent'})
-    comment = new Comment({ content: 'commentContent'})
+    blogPost = new BlogPost({ title: 'blogPostTitle', content: 'blogPostContent' });
+    comment = new Comment({ content: 'commentContent' });
 
     joe.blogPosts.push(blogPost);
     blogPost.comments.push(comment);
     comment.user = joe;
 
-    Promise.all([joe.save(), blogPost.save(), comment.save()])
-      .then(() => done())
+    Promise.all([joe.save(), blogPost.save(), comment.save()]).then(() => done());
   });
 
-  it.only('check save joe blogPost comment with corret relation', (done) => {
-    User.findOne({name: 'joe'})
+  it('check save joe blogPost comment with corret relation', (done) => {
+    User.findOne({ name: 'joe' })
+      .populate('blogPosts')
       .then((user) => {
-        console.log('user: ', user);
+        assert(user.blogPosts[0].title === 'blogPostTitle');
         done();
-      })
-  })
-
+      });
+  });
 });
