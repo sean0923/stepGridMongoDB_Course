839788641c8864c76b532713f1c32801bbb2886b
Sean Hong <iamseanhong@gmail.com>
2018-03-04 17:49:43 -0800

    Implement FindArtist

--- UpStarMusic/database/models/artist.js
+++ UpStarMusic/database/models/artist.js
@@ -14,7 +14,7 @@ const ArtistSchema = new Schema({
   netWorth: Number,
   labelName: String,
   retired: Boolean,
-  Albums: [AlbumSchema],
+  albums: [AlbumSchema],
 });
 
 const Artist = mongoose.model('artist', ArtistSchema);
