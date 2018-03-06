f8a3e5dc3a85b890f3c2e1fad3cdb8e2de27f571
Sean Hong <iamseanhong@gmail.com>
2018-03-06 08:59:05 -0800

    Implement sort in SearchArtist.js

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,14 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Artist.find({}).sort({ [sortProperty]: 1 }).skip(offset).limit(limit);
+
+  return Promise.all([query, Artist.count()]).then((data) => {
+    return {
+      all: data[0],
+      count: data[1],
+      offset,
+      limit,
+    };
+  });
 };
