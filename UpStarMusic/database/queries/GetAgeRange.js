20c38c3c6483f88b875b0d72374de1fcf2da002a
Sean Hong <iamseanhong@gmail.com>
2018-03-05 21:15:56 -0800

    Implement GetYearActiveRange and GetAgeRange

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,15 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minPromise = Artist.find({}).sort({ age: 1 }).limit(1).then((artists) => {
+    return artists[0].age;
+  });
+
+  const maxPromise = Artist.find({}).sort({ age: -1 }).limit(1).then((artists) => {
+    return artists[0].age;
+  });
+
+  return Promise.all([minPromise, maxPromise]).then((data) => {
+    return { min: data[0], max: data[1] };
+  });
 };
--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,15 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minPromise = Artist.find({}).sort({ yearsActive: 1 }).limit(1).then((artists) => {
+    return artists[0].yearsActive;
+  });
+
+  const maxPromise = Artist.find({}).sort({ yearsActive: -1 }).limit(1).then((artists) => {
+    return artists[0].yearsActive;
+  });
+
+  return Promise.all([minPromise, maxPromise]).then((data) => {
+    return { min: data[0], max: data[1] };
+  });
 };
