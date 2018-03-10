5e2c06d78213552dcb88368f588c0cf2c6cdeee5
Sean Hong <iamseanhong@gmail.com>
2018-03-10 12:47:37 -0800

    Add middleware after route to handle err

--- muber/controllers/drivers_controller.js
+++ muber/controllers/drivers_controller.js
@@ -5,19 +5,9 @@ module.exports = {
     res.send({ yeah: 'yeah' });
   },
 
-  create(req, res) {
-    // const driver = new Driver(req.body);
-    // driver.save().then((driver) => {
-    //   res.send(req.driver);
-    // });
-
+  create(req, res, next) {
     Driver.create(req.body)
-      .then((driver) => {
-        res.send(driver);
-      })
-      .catch((err) => {
-        console.log('err: ', err);
-        res.send(err);
-      });
+      .then((driver) => {res.send(driver);})
+      .catch(next);
   },
 };
